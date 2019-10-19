3d71d251bc6f8959024b4e3103996d569a0c8993
Morten <mortenlilbaek@gmail.com>
2019-10-19 08:16:03 +0200

    New location for temp test files

--- src/main/java/org/lilbaek/webstorm/testcafe/helpers/TestUiSessionProviderImpl.java
+++ src/main/java/org/lilbaek/webstorm/testcafe/helpers/TestUiSessionProviderImpl.java
@@ -24,7 +24,7 @@ public class TestUiSessionProviderImpl implements TestUiSessionProvider {
         //Create the additional args required to write the output file
         List<String> list = new ArrayList<>();
         list.add("-r");
-        list.add("spec,json:" + suggestedFile.getName());
+        list.add("spec,json:" + tempFileLocation() + suggestedFile.getName());
         return TestCafeUiSession.create(list, new TestCafeTestFinderStrategy(suggestedFile));
     }
 
@@ -32,7 +32,8 @@ public class TestUiSessionProviderImpl implements TestUiSessionProvider {
      * Creates a suggested temporary output file to write the data to
      */
     private File createSuggestedTempOutputFile() {
-        File tempDir = new File(myProject.getBasePath());
+
+        File tempDir = new File(myProject.getBasePath() + "/" + tempFileLocation());
         String suffix = UUID.randomUUID().toString();
         String fileName = "testcafe-out-" + suffix;
         File tempFile = new File(tempDir, fileName);
@@ -40,5 +41,10 @@ public class TestUiSessionProviderImpl implements TestUiSessionProvider {
         // the application exits before then.
         tempFile.deleteOnExit();
         return tempFile;
+    }
+
+    @NotNull
+    private String tempFileLocation() {
+        return "node_modules/testcafe/tmp/";
     }
 }
